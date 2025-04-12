167785e0c8d7a7cb06e15bac4c52253faafc576b
KaptainMidnight <artemkin030@gmail.com>
2025-04-13 01:16:16 +0400

    Added testing skeleton

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KaptainMidnight\TelegramAuthenticator\Tests\Unit;
+
+use Orchestra\Testbench\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
